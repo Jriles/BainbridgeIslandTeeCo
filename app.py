2ebe22d5be6a21c94aa7b4b604bcc982bcc4d42e
Jack Riley <jriley9000@gmail.com>
2020-06-03 15:35:26 -0700

    change to where subprocess is opened

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def github_webhook_endpoint():
     app.logger.info(request_data)
     # now we want to run our .sh file in our home page
     import subprocess
-    subprocess.Popen('chmod +x deploy.sh', shell=True)
+    subprocess.Popen('/home/ubuntu/BainbridgeIslandTeeCo/deploy.sh', shell=True)
     app.logger.info("finished running the command")
     return "Okay, thank you, if you still care."
 
