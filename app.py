424963d06788672f46f29dec59f787f920c3e7c5
Jack Riley <jriley9000@gmail.com>
2020-06-03 16:47:42 -0700

    change to permissions for subprocess

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def github_webhook_endpoint():
     # now we want to run our .sh file in our home page
     import subprocess
     from subprocess import PIPE
-    process = subprocess.Popen('/home/ubuntu/BainbridgeIslandTeeCo/deploy.sh', shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    process = subprocess.Popen('chmod a+x /home/ubuntu/BainbridgeIslandTeeCo/deploy.sh', shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     try:
         outs, errs = process.communicate(timeout=15)
     except TimeoutExpired:
