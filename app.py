cbcabd8af372405bee9a5a09ffd46291aefde297
Jack Riley <jriley9000@gmail.com>
2020-06-05 20:30:42 -0700

    change to webhook subprocess

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def github_webhook_endpoint():
     from subprocess import PIPE
     current_directory = os.path.dirname(os.path.realpath(__file__))
     #app.logger.info("current dir: " + current_directory)
-    process =subprocess.Popen('git pull origin master', cwd="/home/ubuntu/BainbridgeIslandTeeCo", universal_newlines=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    process =subprocess.Popen('/usr/bin/git pull origin master', cwd="/home/ubuntu/BainbridgeIslandTeeCo", universal_newlines=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     app.logger.info(process.stdout.read())
     #from git import Repo
     #repo = Repo("/home/ubuntu/BainbridgeIslandTeeCo")
