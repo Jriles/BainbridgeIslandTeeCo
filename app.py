f89a3f758c998edac21358d4ca6afd1303dae1d2
Jack Riley <jriley9000@gmail.com>
2020-06-23 09:44:15 -0700

    made it so that we can push code even if maintenance mode is on

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ def turn_off_mode():
 @app.before_request
 def check_for_maintenance():
     maintenance_status = MaintenanceMode.query.first()
-    if current_user.is_authenticated is False and maintenance_status.status == "On" and request.path != url_for('maintenance') and request.path != url_for('login') and "js" not in request.path and "css" not in request.path:
+    if current_user.is_authenticated is False and maintenance_status.status == "On" and request.path != url_for('maintenance') and request.path != url_for('login') and "js" not in request.path and "css" not in request.path and "myhook" not in request.path:
         return redirect(url_for('maintenance'))
         # Or alternatively, dont redirect
         # return 'Sorry, off for maintenance!', 503
