b853abf5343f5b29bf1f000fbb8295359950022a
Jack Riley <jriley9000@gmail.com>
2020-06-23 21:22:52 -0700

    changes to scroll in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -969,7 +969,7 @@ function scrollDown(product){
             }
         }
         console.log(found_product_index);
-        var relevant_product_y_pos = document.getElementById("primary-product-image" + String(found_product_index)).getBoundingClientRect().top;
+        var relevant_product_y_pos = document.getElementById("primary-product-image" + String(found_product_index)).scrollTop;
         console.log(relevant_product_y_pos);
         //access the product at index and get its y position on the page
         if(i !== -1){
