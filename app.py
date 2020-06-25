4d60f9533e5452132984b1e2d4c900471413e8ec
Jack Riley <jriley9000@gmail.com>
2020-06-24 23:10:30 -0700

    fixes to product id in edit product form

--- app.py
+++ app.py
@@ -577,7 +577,7 @@ def edit_products():
                 this_design.design_icon = "static/img/" + icon_file_name
             db.session.commit()
     app.logger.info("NEW ORDER ARRAY: " + str(edit_product_order.new_order_array.data))
-    if edit_product_order.new_order_array.data is not None and edit_product_order.validate():
+    if edit_product_order.new_order_array.data is not None:
         new_order_id_arr = edit_product_order.new_order_array.data.split(',')
         app.logger.info("REORDER ARRAY[1]: " + str(new_order_id_arr))
         for idx, this_id in enumerate(new_order_id_arr):
