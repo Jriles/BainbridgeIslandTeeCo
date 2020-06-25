d753673c8b7ca620fa0b2943632bec66bac5ec0c
Jack Riley <jriley9000@gmail.com>
2020-06-25 13:31:28 -0700

    fixing design selection dots

--- static/js/main.js
+++ static/js/main.js
@@ -777,7 +777,7 @@ $('.owl-carousel').on('changed.owl.carousel', function(event) {
         var design_names = $(event.target).parent().parent().find(".design_names");
         console.log(design_names.get(0).children);
         var this_design_name = $(event.target).parent().parent().find(".design-name");
-        console.log("design_names: " + design_names);
+        console.log("design_names.get(0): " + design_names.get(0));
         this_design_name.html(design_names.get(0).children[index].innerHTML);
     }
 })
