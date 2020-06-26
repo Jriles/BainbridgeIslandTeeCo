5c199bce1c934a945f06008a49d938f7d5f63c95
Jack Riley <jriley9000@gmail.com>
2020-06-26 08:20:45 -0700

    fixing design selection dots

--- static/js/main.js
+++ static/js/main.js
@@ -644,7 +644,7 @@ function addToCart(button){
     console.log(size);
     var productName = String($(thisProductInfo).find("#product-name").text());
     var productPrice = String($(thisProductInfo).find("#product-price").text());
-    var thisProductImage = $(thisProductInfo).find(".primage-image").src;
+    var thisProductImage = $(thisProductInfo).find(".img-fluid.primage-image").src;
     var thisProductDesign = $(thisProductInfo).find('.design-name').text();
     console.log(thisProductImage);
     var newProduct = {"ProductName": productName,"Size": size,"Price": productPrice, "Quantity": String(quantity), "IMGSRC": thisProductImage, "Design": thisProductDesign};
