f131169a641068c4eb39d46e84ace52b272d62d4
Jack Riley <jriley9000@gmail.com>
2020-06-26 14:42:05 -0700

    fixing design selection dots

--- static/js/main.js
+++ static/js/main.js
@@ -776,17 +776,12 @@ $('.owl-carousel').on('changed.owl.carousel', function(event) {
 
     //we also want to reset which design icon we are highlighting
     var design_buttons = $(event.target).closest(".s_product_inner").find('#design-selection').children();
-    console.log(design_buttons);
     for(i = 0; i < design_buttons.length;i++){
         design_buttons[i].style.border = "none";
     }
 
     var design_names = $(event.target).parent().parent().find(".design_names");
-    console.log(design_names.get(0).children);
     var this_design_name = $(event.target).parent().parent().find(".design-name");
-    console.log("design_names.get(0): " + design_names.get(0));
-    console.log("design_names.get(0).children[index]: " + design_names.get(0).children[index]);
-    console.log("index: " + index);
     if(design_names.get(0).children[index] !== undefined){
         this_design_name.html(design_names.get(0).children[index].innerHTML);
         design_buttons[index].style.border = "2px solid green";
