d783afae7ef01aaf54f9b428afb9d809eb5432c0
Jack Riley <jriley9000@gmail.com>
2020-06-26 22:50:06 -0700

    fixing design selection dots

--- app.py
+++ app.py
@@ -321,6 +321,7 @@ def inject_logo():
         path = last_item.file_path
     primary_color = SitePrimaryColor.query.first()
     primary_color = primary_color.color
+    app.logger.info(primary_color)
     return dict(this_file_path=path,
                 nav_products=get_display_products_in_order(),
                 primary_color=primary_color)
