c248252faef189c0734cd43afd9cb7cb0afc9e9b
Jack Riley <jriley9000@gmail.com>
2020-06-27 08:38:13 -0700

    fixing design selection dots

--- static/js/main.js
+++ static/js/main.js
@@ -1030,6 +1030,7 @@ function goToSlide(button){
     var design_buttons = button.parentElement.children;
     console.log("design_buttons.length: " + design_buttons.length);
     for(i = 0; i < design_buttons.length;i++){
+        console.log(design_buttons[i]);
         design_buttons[i].style.border = "none";
     }
     console.log("blanked design buttons");
