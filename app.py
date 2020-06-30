382005df175a441230fc58e41c1fe9c0bba06242
Jack Riley <jriley9000@gmail.com>
2020-06-29 17:42:55 -0700

    logging sending mail

--- app.py
+++ app.py
@@ -413,6 +413,11 @@ def paymentsuccess():
         db.session.add(this_email)
         db.session.commit()
     email = request.form['Email']
+    #add this email to our customer email pool
+    this_email = Email()
+    this_email.email = email
+    db.session.add(this_email)
+    db.session.commit()
     paypalID = request.form['PayPalTransactionID']
     orderDeets = paypalstuff.GetOrder.get_order(paypalstuff.GetOrder, paypalID)
     address = orderDeets.result.purchase_units[0].shipping.address
