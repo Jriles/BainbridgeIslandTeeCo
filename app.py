6177988cb3b2d944f4264434b7069067d515f83d
Jack Riley <jriley9000@gmail.com>
2020-07-02 15:51:18 -0700

    more changes to user roles

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), primary_key=True)
     email_confirmed_at = datetime.datetime.now()
     password = db.Column(db.String(255))
-    roles = db.relationship('Role', cascade='save-update', backref='Users', secondary='User_Roles')
+    roles = db.relationship('Role', backref='Users')
     active = True
     name = db.Column(db.String(255))
     id = db.Column(db.String(255))
@@ -167,8 +167,8 @@ class Role(db.Model):
 class UserRoles(db.Model):
     __tablename__ = 'User_Roles'
     id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.String(), db.ForeignKey('Users.email', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer(), db.ForeignKey('Roles.id', ondelete='CASCADE'))
+    user_id = db.Column(db.String(), db.ForeignKey('Users.email'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('Roles.id'))
 
 
 class Email(db.Model):
