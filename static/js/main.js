a9048c0848f0905a70dafdf81a14c865bfc1d177
Jack Riley <jriley9000@gmail.com>
2020-07-07 08:31:20 -0700

    fixing issues with last big push

--- static/js/main.js
+++ static/js/main.js
@@ -1108,7 +1108,8 @@ function checkDesignSizeInventory(product_element_id){
         var current_design = $(".product-design-icon.active")
         var current_design_index = $(".product_design_icon.active").index();
         console.log("current design index: " + current_design_index);
-        var current_design_inventory_count = String($(current_product).find(".design_inventories").get(0).children[current_design_index]);
+        //var current_design_inventory_count = String($(current_product).find(".design_inventories").get(0).children[current_design_index]);
+        var current_design_inventory_count = String($(current_product).find(".design_inventories").get(0);
         console.log("current design inventory count: " + current_design_inventory_count);
         if(current_design_inventory_count > 0){
             in_stock = true;
