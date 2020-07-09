5aa7e5f19c78822c4489b61eb23dfdd3c21be104
Jack Riley <jriley9000@gmail.com>
2020-07-08 21:10:14 -0700

    changes to homepage js

--- static/js/main.js
+++ static/js/main.js
@@ -1109,10 +1109,11 @@ function checkDesignSizeInventory(product_element_id){
     //if a size is selected show if in stock
     var current_product = document.getElementById(product_element_id);
     console.log($(current_product).find(".owl-dots"));
-    var dots = $("#" + String(product_element_id)).find(".owl-dots").get(0);//.childNodes;
+    var dots = $("#" + String(product_element_id)).find(".owl-dot.active");//.childNodes;
     console.log(dots);
     var dot_count = $(dots).children().length;
     console.log("dot count: " + dot_count);
+
     /*
     var current_size_drop_down = $(current_product).find("#t-shirt-size" + String(current_design_index))[0];
     console.log("current drop down children length: " + current_size_drop_down);
