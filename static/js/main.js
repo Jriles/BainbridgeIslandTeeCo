99f5d0e0313aab9808dd7459bc7a4dfccf5d2c38
Jack Riley <jriley9000@gmail.com>
2020-07-10 10:42:11 -0700

    changes to homepage js

--- static/js/main.js
+++ static/js/main.js
@@ -1067,6 +1067,22 @@ function checkDesignSizeInventory(product_element_id){
 
 $('.slick-carousel').on('afterChange', function(event, slick, currentSlide){
     console.log(currentSlide);
+    //we want to ask if there is a primary image first
+    console.log($(button).closest(".product_image_area").find(".primary-image"));
+    var design_index = currentSlide;   //we want to determine what design we want to show
+    //if there is a primary product image
+    if($(event.target).closest(".product_image_area").find(".primary-image").length > 0){
+        //there is a primary product image
+        //first we want to ask if the index is zero
+        //0 -> 0
+        //1 -> 0
+        //2 -> 1
+        //etc. essentially if not zero subtract one. But only if there is a primary product image'
+        if(currentSlide !== 0){
+            design_index--;
+        }
+    }
+    //else there is no primary product image and we simply do nothing
 });
 
 function inventorySizeCheckWrapper(dropdown){
