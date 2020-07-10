b11649d91053ef63631cf435c16ded80e96a1c6d
Jack Riley <jriley9000@gmail.com>
2020-07-09 20:40:40 -0700

    changes to homepage js

--- static/js/main.js
+++ static/js/main.js
@@ -311,8 +311,8 @@ $(function() {
             console.log("primary color: " + primary_color);
             console.log($(".slick-prev:before"));
             console.log($(".slick-next:before"));
-            $(".slick-prev:before").css("color", String(primary_color));
-            $(".slick-next:before").css("color", String(primary_color));
+            $(".slick-prev:before").css("color", String(primary_color) + "!important;");
+            $(".slick-next:before").css("color", String(primary_color) + "!important;");
             console.log($(".slick-prev:before"));
             console.log($(".slick-next:before"));
 		});
