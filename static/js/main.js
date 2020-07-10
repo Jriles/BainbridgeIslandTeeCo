d90f3813de30e53e6a1fccbe61f7c41ec6840f93
Jack Riley <jriley9000@gmail.com>
2020-07-10 11:07:29 -0700

    changes to homepage js

--- static/js/main.js
+++ static/js/main.js
@@ -1091,11 +1091,12 @@ $('.slick-carousel').on('afterChange', function(event, slick, currentSlide){
     $(current_icon).css("border", ("2px solid" + primary_color));
 
     //now we want to set the name of the design shown
-    var design_names = $(product_area).find(".design_names");
+    var design_detail_li = $(product_area).find(".design_size_list").eq(design_index);
     console.log(design_names);
-    var current_design_name = $(design_names).children().eq(design_index).children().eq(0);
+
+    var current_design_name = $(design_detail_li).children().eq(0);
     console.log(current_design_name);
-    $(design_names).children().hide();
+    //$(design_names).children().hide();
     $(current_design_name).show();
 });
 
