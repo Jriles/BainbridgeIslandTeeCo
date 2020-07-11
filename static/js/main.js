aaeb2615ff935a2298e6c79e190e6c500fa77e31
Jack Riley <jriley9000@gmail.com>
2020-07-11 10:47:49 -0700

    changes to homepage js

--- static/js/main.js
+++ static/js/main.js
@@ -1088,7 +1088,7 @@ function goToSlide(button){
     var product_area = $(button).closest(".product_image_area");
     var current_carousel = $(product_area).find('.slick-carousel');
     console.log("design icon index: " + $(button).index());
-    $(current_carousel).slick('slickGoTo', 'index', $(button).index());
+    $(current_carousel).slick('slickGoTo', 'index', $(button).index(), 'dontAnimate', false);
 }
 
 function inventorySizeCheckWrapper(dropdown){
