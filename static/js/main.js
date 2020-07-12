4db80dec7c1ecb5cbd18daa9cd903632bd182897
Jack Riley <jriley9000@gmail.com>
2020-07-11 18:13:09 -0700

    making it so that admins can reorder sizes

--- static/js/main.js
+++ static/js/main.js
@@ -313,9 +313,9 @@ $(function() {
                         for(i = 0; i < arr.length;i++){
                             new_size_order_id_arr.push($(arr[i]).find("#size_id").val());
                         }
-                        console.log(new_order_id_arr);
+                        console.log(new_size_order_id_arr);
 
-                        $(table.parentElement).find("#new_size_order_arr").val(String(new_order_id_arr));
+                        $(table.parentElement).find("#new_size_order_arr").val(String(new_size_order_id_arr));
                         console.log($(table).parent().find("#size-reordering-form"));
                         $(table).parent().find("#size-reordering-form").submit();
                     }
