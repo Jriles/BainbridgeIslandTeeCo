fb98c48e8f8931d23501357ab3107fa931bfe293
Jack Riley <jriley9000@gmail.com>
2020-11-06 07:37:11 -0800

    aligning with dev

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ app.config['USER_APP_NAME'] = 'Alex apparel website'
 app.config['USER_ENABLE_EMAIL'] = True
 app.config['USER_ENABLE_USERNAME'] = False
 app.config['USER_REQUIRE_RETYPE_PASSWORD'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:843134@localhost:5432/teecotest"
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.environ['PROD_DB_CONN_STR'])
 app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 587
 
